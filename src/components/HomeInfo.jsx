2f53ab77402b2eb098386e907726c620cb995c40
idris kulubi <137810838+169398@users.noreply.github.com>
2024-04-09 15:19:47 +0300

    Update HomeInfo.jsx
    Minor change
    😊

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -10,7 +10,7 @@ const HomeInfo = ({ currentStage }) => {
         <span className='font-semibold mx-2 text-white'>Idris</span>
         👋
         <br />
-        A Software Engineer from Kenya 🇰🇪
+        A Software Developer from Kenya 🇰🇪
       </h1>
     );
 
